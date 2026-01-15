15205c96ecc7c8a2150da7308572ee0b1fc6dda7
CosimoIurlaro <cos.iurlaro@gmail.com>
2026-01-15 19:30:36 +0100

    fix CORS on render

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://localhost:5173",
         "https://portale-prin.vercel.app",
     ],
-    allow_origin_regex=r"^https://.*\.vercel\.app$",
+    #allow_origin_regex=r"^https://.*\.vercel\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
