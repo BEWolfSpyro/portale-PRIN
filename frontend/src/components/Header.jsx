9d9a46c4383f1f2bdc6689c54cff2e5cc457948e
CosimoIurlaro <cos.iurlaro@gmail.com>
2026-01-21 19:03:55 +0100

    Mobile adaptation of header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -38,36 +38,32 @@ export default function Header({
           </div>
         </div>
 
-{/* MOBILE */}
-<div className="md:hidden flex flex-col items-center text-center">
-  
-  {/* Titolo */}
-  <div className="text-lg font-semibold leading-snug">
-    Portal PRIN 2022 - SCORET
-  </div>
+	{/* MOBILE */}
+        <div className="md:hidden flex flex-col items-center text-center">
+        {/* Titolo */}
+	<div className="text-lg font-semibold leading-snug">
+            Portal PRIN 2022 - SCORET
+          </div>
 
-  {/* Logo sinistra */}
-  <img
-    src="/logos/logo-sinistra.png"
-    alt="Logo sinistra"
-    className="mt-4 h-14 object-contain"
-  />
-
-  {/* Logo destra */}
-  <img
-    src="/logos/logo-destra.png"
-    alt="Logo destra"
-    className="mt-3 h-14 object-contain"
-  />
-
-  {/* Bottone */}
-  <a
-    href={rightLinkHref}
-    className="mt-4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-[#2f6fb6] shadow-sm hover:bg-slate-50"
-  >
-    {rightLinkLabel}
-  </a>
-</div>
+	{/* Logo Sinistra */}
+            <img
+              src="/logos/nextgeneu1.png"
+              alt="Logo sinistra"
+              className="mt-4 h-12 object-contain"
+            />
+	{/* Logo Destra */}
+            <img
+              src="/logos/Italiadomani1.png"
+              alt="Logo destra"
+              className="mt-3 h-12 object-contain"
+            />
+          
+           <a
+              href={rightLinkHref}
+              className="mt-4 rounded-md bg-white px-4 py-2 text-sm font-semibold text-[#2B65AF] shadow-sm hover:bg-slate-50"
+            >
+              {rightLinkLabel}
+            </a>
         </div>
       </div>
     </header>
